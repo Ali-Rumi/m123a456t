7879f62f1412e4d69c12bda9bbe23d8d08f2cb6b
Ali-Rumi <iamalirazamh@gmail.com>
2024-08-26 12:32:49 +0500

    Update matic_short.py

--- matic_short.py
+++ matic_short.py
@@ -4,8 +4,8 @@ import math
 import time
 
 # API credentials
-api_key = '7D10dWr9jWGLWP5rfAXjcXqijGPveCk5tFhKON3SCyk1hkSZdtq1DOJJv8eaTu01'
-api_secret = 'Rb0j7IP52yNoKKj1eMwwfaYxuRtOpLfJmorp9yGdJW7T2iLRLdKaPCsvwjxuRB0G'
+api_key = 'FOfIYCxqYfKRHBCcT4j9xJjkMa1IL7tkMpvSqOcPrlllIpvt59YjVkpgyoZbr7rS'
+api_secret = 'KOsx97evpt2etVZxpZzq1VKlzPOekTR67WASawHz04rmfesUoPNCZJ1f3QT9bdPU'
 
 # Initialize the UMFutures client
 client = UMFutures(key=api_key, secret=api_secret)
